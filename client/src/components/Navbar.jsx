a57be97220877e6f52fbdf8a61206a6496eca24d
Mephisto Aliff <aliff.ibr@icloud.com>
2023-03-19 02:23:39 +0800

    test

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,10 +7,10 @@ import {
   SettingsOutlined,
   ArrowDropDownOutlined,
 } from '@mui/icons-material';
-import FlexBetween from 'components/FlexBetween';
+import FlexBetween from './FlexBetween';
 import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
-import profileImage from 'assets/profile.jpg';
+import profileImage from '../assets/profile.jpg';
 import {
   AppBar,
   Button,
