4bf7acbd7048a66bbcaa4271030728d47da577b3
Mephisto Aliff <aliff.ibr@icloud.com>
2023-03-19 19:38:45 +0800

    added product features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { themeSettings } from './theme';
-import Layout from 'scenes/layout';
-import Dashboard from 'scenes/dashboard';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -19,6 +20,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,15 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001' }),
-  reducerPath: 'adminApi',
-  tagTypes: ['User'],
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001" }),
+  reducerPath: "adminApi",
+  tagTypes: ["User"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
-      providesTags: ['User'],
+      providesTags: ["User"],
+    }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
     }),
   }),
 });
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
